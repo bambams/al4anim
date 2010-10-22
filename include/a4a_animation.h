63138a550cab4ce67e9df016a216f44846b9b8d6
Brandon McCaig <bamccaig@gmail.com>
2010-10-22 13:55:00 -0400

    Adding licensing.

--- include/a4a_animation.h
+++ include/a4a_animation.h
@@ -1,3 +1,23 @@
+/*
+ * al4anim is a simple animation library for Allegro 4.
+ * Copyright (C) 2009 Brandon McCaig
+ *
+ * This file is part of al4anim.
+ *
+ * al4anim is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * al4anim is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with al4anim.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef ANIMATION_H
     #define ANIMATION_H
 
